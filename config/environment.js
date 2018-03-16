39e7add7d73d71bb3646c20a8fc81fd1f3994b06
Matthew J Morrison <mattjmorrison@mattjmorrison.com>
2018-03-15 22:23:37 -0500

    Bug Fix: update root url for github pages

--- config/environment.js
+++ config/environment.js
@@ -6,7 +6,7 @@ module.exports = function(environment) {
     modulePrefix: 'mattjmorrison-ember',
     locationType: 'none',
     environment,
-    rootURL: '/',
+    rootURL: '/mattjmorrison-ember',
     EmberENV: {
       FEATURES: {
       },
