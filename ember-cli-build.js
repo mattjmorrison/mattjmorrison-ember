150cbee66fd44ba8e004d4aecef1c104058e1321
Matthew J Morrison <mattjmorrison@mattjmorrison.com>
2018-03-15 23:26:40 -0500

    Peripheral: remove config from meta

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,6 +5,7 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
+    storeConfigInMeta: false,
     fingerprint: {
       enabled: false
     },
